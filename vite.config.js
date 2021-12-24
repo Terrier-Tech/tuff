4aab4406b4037b3ff97975b5c105eb84f485bef0
Andy Selvig <ajselvig@gmail.com>
2021-12-24 13:35:26 -0600

    moved the counter demo to the root, fixed vite production build I guess?

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,13 @@ module.exports = defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'src/tuff/main.ts'),
       name: 'Tuff',
+      formats: ['es'],
       fileName: (format) => `tuff.${format}.js`
     },
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html'),
-        counter: path.resolve(__dirname, 'src/tuff-demo/counter.html')
+        counter: path.resolve(__dirname, 'counter.html')
       }
     }
   }
