5126075adc6835a495013663de173bf1d787148b
Andy Selvig <ajselvig@gmail.com>
2022-01-22 15:54:27 -0600

    export logger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import * as logger from './logger'
 import * as parts from './parts'
 import * as messages from './messages'
 import * as tags from './tags'
 import * as keyboard from './keyboard'
 
-export {parts, messages, tags, keyboard}
+export {logger, parts, messages, tags, keyboard}
