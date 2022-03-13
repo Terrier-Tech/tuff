25fb64b95357389ce5bf5fcb2c6c3c78ab097ee9
Andy Selvig <ajselvig@gmail.com>
2022-03-13 09:58:25 -0500

    added vitest and some string tests, added strings.camelCase, made all string functions share the same word splitting regex

--- src/strings.ts
+++ src/strings.ts
@@ -1,4 +1,16 @@
 
+
+const wordRegex = /(?<=[a-z\d])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|\s+|_+|-+/g
+
+/**
+ * Splits a string into words (based on spaces, dashes, underscores, or capitalization)
+ * @param s A string to split
+ * @returns The string split into words
+ */
+export function splitWords(s: string): Array<string> {
+    return s.split(wordRegex)
+}
+
 /**
  * Capitalize only the first word of a string.
  * @param word - A string to capitalize
@@ -11,15 +23,13 @@ export function capitalize(word: string) {
 
 /**
  * Capitalize the first letter of every word in the string.
- * @param word - A string to titleize
- * @returns - The titleized word
+ * @param s - A string to titleize
+ * @returns - The titleized string
  */
-export function titleize(word: string) {
-    if (!word) return word;
-    return word.split(/\s+/g).map(s => {return capitalize(s)}).join(' ')
+export function titleize(s: string) {
+    if (!s) return s;
+    return splitWords(s).map(s => {return capitalize(s)}).join(' ')
 }
-
-const wordRegex = /(?<=[a-z\d])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])/g
 
 /**
  * Converts a camelCase string to a rope-cased one.
@@ -27,7 +37,18 @@ const wordRegex = /(?<=[a-z\d])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])/g
  * @returns The rope-cased string
  */
 export function ropeCase(s: string) {
-    return s.split(wordRegex).map(w => {
+    return splitWords(s).map(w => {
         return w.toLowerCase()
     }).join('-')
+}
+
+/**
+ * Converts a rope, space, or underscore delimited string into camelCase.
+ * @param s The string to convert
+ * @returns The camelCase version of the string
+ */
+export function camelCase(s: string) {
+    return splitWords(s).map((w, i) => {
+        return i == 0 ? w.toLocaleLowerCase() : capitalize(w)
+    }).join('')
 }
---
+++ src/test/strings.test.ts
@@ -0,0 +1,27 @@
+import { expect, test } from 'vitest'
+import * as strings from '../strings'
+
+test("splitWords", () => {
+    expect(strings.splitWords("hello world-fooBar")).toMatchObject(['hello', 'world', 'foo', 'Bar'])
+})
+
+test("capitalize", () => {
+    expect(strings.capitalize("hello world")).toBe("Hello world")
+})
+
+test("titleize", () => {
+    expect(strings.titleize("hello world")).toBe("Hello World")
+    expect(strings.titleize("hello_world")).toBe("Hello World")
+})
+
+test("ropeCase", () => {
+    expect(strings.ropeCase("foo bar")).toBe("foo-bar")
+    expect(strings.ropeCase("FooBar")).toBe("foo-bar")
+    expect(strings.ropeCase("foo_bar")).toBe("foo-bar")
+})
+
+test("camelCase", () => {
+    expect(strings.camelCase("foo bar")).toBe("fooBar")
+    expect(strings.camelCase("Foo-Bar")).toBe("fooBar")
+    expect(strings.camelCase("foo_bar")).toBe("fooBar")
+})
