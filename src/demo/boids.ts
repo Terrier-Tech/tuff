3cfe92f3423395722002b3cd78e97d8828e0c060
Eric Isaacson <eric@actsmedia.com>
2022-04-10 00:48:37 -0500

    boids wrap around responsive screen

--- src/demo/boids.ts
+++ src/demo/boids.ts
@@ -7,141 +7,119 @@ import {arrays} from "../main";
 const log = new logging.Logger('Boids')
 
 const colors= ['#0088aa', '#00aa88', '#6600dd']
-const areaHeight= 500 // Height of svg container
+const areaHeight= '500px' // Height of svg container
 const numBoids= 1 // the number of each boid to generate
-const fps= 10;
-
-function genPos(): number {
-    return 0
-}
-
-function genRotation(): number {
-    return 0// Math.random()*360
-}
+const boidVelocity=20; // in px per second
 
 // TODO
 class Boid  {
 
-    x = genPos()
-    y = genPos()
-    velocity=3
-    rotation = genRotation()
+    x: number
+    y: number
+    velocity=boidVelocity
+    rotation = Math.random()*360
     color= arrays.sample(colors)
+    containerEl: HTMLElement
 
-    constructor(
-        readonly id = demo.newId()
-    ) {}
+    rotationRadians = () => this.rotation * Math.PI / 180.0
 
-    rotationRadians = () => {
-        return (this.rotation * Math.PI) / 180.0;
+    constructor(containerEl: HTMLElement, readonly id = demo.newId()) {
+        this.containerEl=containerEl;
+        this.x = this.containerEl.offsetWidth / 2;
+        this.y = this.containerEl.offsetHeight / 2;
     }
 
     nextPos = () => {
         this.x = (this.x + Math.cos(this.rotationRadians()) * this.velocity)
         this.y = (this.y + Math.sin(this.rotationRadians()) * this.velocity)
+        if (this.x < 0) this.x += this.containerEl.offsetWidth;
+        if (this.y < 0) this.y += this.containerEl.offsetWidth;
     }
 
-    render = (svg) => {
-        const parentEl= document.getElementById('svg-container')
-        if (!parentEl) { return svg.path({})}
-
+    render = (svg: SVGElement) => {
         // to wrap boids around on collision with boundary
-        const modX= parentEl.offsetWidth / 2; // divide by 2 because svg origin is centered
-        const modY= parentEl.offsetHeight / 2; // divide by 2 because svg origin is centered
-
-        console.log(`${this.x}, ${this.y}`)
-        console.log(`${modX}, ${modY}`)
-        console.log(``)
-
+        const renderX= Math.floor(this.x % this.containerEl.offsetWidth );
+        const renderY= Math.floor(this.y % this.containerEl.offsetHeight );
         svg.path({
             id: this.id,
             fill:this.color,
             stroke: this.color,
             strokeWidth: '2',
-            transform: `rotate(${ this.rotation }, ${ this.x }, ${ this.y })`,
-            d: `M${ this.x } ${ this.y }, l-10 2, v-4 l10 2`
+            d: `M${ renderX  } ${ renderY }, l-10 2, v-4 l10 2`,
+            transform: `rotate(${ this.rotation }, ${ renderX }, ${ renderY })`,
         })
     }
-
 }
 
 export class Inputs extends Part<{}> {
-
     render(parent: PartTag) {
         parent.h1().text("INPUTS")
     }
 }
 
-export class SVG extends Part<{}> {
+export class SVG extends Part<{containerEl: HTMLElement}> {
 
-    boids: Boid[] = arrays.range(0, numBoids-1).map( () => new Boid() )
+    boids: Boid[] = []
 
     init() {
-        setInterval(this.mainLoop, 1000 / fps)
+        this.boids= arrays.range(0, numBoids-1).map( () => new Boid(this.state.containerEl) )
+        setInterval(this.mainLoop, 1000 / boidVelocity)
     }
 
     mainLoop = () => {
-        for (let boid of this.boids) boid.nextPos()
+        this.boids.forEach(b => b.nextPos())
         this.dirty()
     }
 
-    svgAttrs = () => {
-        const parentEl= document.getElementById('svg-container')
-        if (!parentEl) { return { height: areaHeight} }
+    containerWidth = () => this.state.containerEl.offsetWidth
+    containerHeight = () => this.state.containerEl.offsetHeight
 
-        let width = parentEl.offsetWidth;
-        let height = parentEl.offsetHeight;
+    svgAttrs = () => ({
+        width: '100%',
+        height: '100%',
+        viewBox: {
+            width: this.containerWidth(),
+            height: this.containerHeight() }})
 
-        return {
-            width: '100%',
-            height: areaHeight,
-            viewBox: {
-                width: width,
-                height: height,
-                // center svg origin
-                x: -1 * width / 2,
-                y: -1 * height / 2,
-            }
-        }
-    }
-
-    renderCentroid = (svg) => {
-        const x= this.boids.reduce((sum, b) => sum + b.x, 0) / this.boids.length
-        const y= this.boids.reduce((sum, b) => sum + b.y, 0) / this.boids.length
-        return svg.circle({cx: x, cy: y, r: 5, fill: 'magenta'}).css({opacity: '0.75'})
-    }
+    renderCentroid = (svg) =>
+        svg.circle({
+            cx: this.boids.reduce((sum, b) => sum + b.x, 0) / this.boids.length,
+            cy: this.boids.reduce((sum, b) => sum + b.y, 0) / this.boids.length,
+            fill: 'magenta', r: 5 }).css({opacity: '0.75'})
 
     renderOrigin = (svg) =>
-        svg.circle({cx: 0, cy: 0, r: 10, stroke: 'cyan', fill: 'cyan'})
+        svg.circle({
+            cx: this.containerWidth() / 2,
+            cy: this.containerHeight() / 2,
+            r: 10, stroke: 'cyan', fill: 'cyan'})
 
     render(parent: PartTag) {
         parent.svg( svg => {
-            // svg.css({backgroundColor: 'blue'})
             svg.attrs(this.svgAttrs())
             this.boids.forEach(b => b.render(svg))
             this.renderCentroid(svg)
-            this.renderOrigin(svg)
-        })
-    }
+            this.renderOrigin(svg) })}
 }
 
 export class App extends Part<{}> {
 
-    svg!: SVG
     inputs!: Inputs
 
     init() {
-        this.svg= this.makePart(SVG, {})
         this.inputs= this.makePart(Inputs, {})
     }
 
     render(parent: PartTag) {
         parent.div(styles.flexRow, styles.padded, d => {
-            d.div('#input-container', styles.contentInset, styles.padded, d => {
-                d.part(this.inputs) })
-            d.div('#svg-container',styles.contentInset, styles.flexStretch, d => {
-                d.part(this.svg) })
+            d.div('#input-container', styles.contentInset, styles.padded, d => { d.part(this.inputs) })
+            d.div('#svg-container','.svg-container', styles.contentInset, styles.flexStretch)
+                .css({ height: areaHeight })
         })
+    }
+
+    update(elem: HTMLElement) {
+        const svgContainer= elem.getElementsByClassName("svg-container")[0]
+        Part.mount(SVG, svgContainer, {containerEl: svgContainer})
     }
 }
 
