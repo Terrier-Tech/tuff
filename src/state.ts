f888ff4fc179466c4df871b8f8108b7f7b006bfb
Andy Selvig <ajselvig@gmail.com>
2022-04-15 16:20:30 -0500

    minor state change

--- src/state.ts
+++ src/state.ts
@@ -10,9 +10,9 @@ export type StateParser = Record<string, Parser<any>>
 
 /**
  * Maps the type of a state parser into the type of the corresponding state, e.g.:
- * const stateMap = {id: stringParser, num: intParser}
- * state.Map<typeof stateMap>
- * // => type {id: string, num: number}
+ * const stateParser = {id: stringParser, num: intParser}
+ * state.MapParser<typeof stateParser>
+ * // => {id: string, num: number}
  */
 export type MapParser<SP extends StateParser> = {
     [K in keyof SP]: ReturnType<SP[K]["parse"]>
