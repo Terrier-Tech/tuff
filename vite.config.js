fdeeb1399a3b8c13d66bc51082d6223161166b32
Andy Selvig <ajselvig@gmail.com>
2022-04-30 10:10:46 -0500

    generate a single type file for distribution

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import * as path from 'path'
 import {defineConfig} from 'vite'
 import { fileURLToPath } from 'url'
 
-// Can't get multiple modules to work with typescript yet
-// import { folderInput } from 'rollup-plugin-folder-input'
-
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
@@ -15,21 +12,12 @@ export default defineConfig({
     vanillaExtractPlugin()
   ],
   build: {
+    emptyOutDir: false,
     lib: {
       entry: path.resolve(dirname, 'src/main.ts'),
       name: 'Tuff',
       formats: ['es'],
       fileName: (format) => `tuff.${format}.js`
     }
-    // rollupOptions: {
-    //   input: 'src/*.ts',
-    //   output: {
-    //     entryFileNames: '[name].js',
-    //     dir: './dist'
-    //   },
-    //   plugins: [
-    //     folderInput()
-    //   ]
-    // }
   },
 })
