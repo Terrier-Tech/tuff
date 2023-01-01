397162058bd672505ff7aa92944f749343687d26
Andy Selvig <ajselvig@gmail.com>
2023-01-01 09:13:28 -0600

    tests for mat.multiply

--- src/test/mat.test.ts
+++ src/test/mat.test.ts
@@ -29,6 +29,28 @@ test("matrix scaling", () => {
     expect(v1.y).approximately(3, epsilon)
 })
 
+function expectMatEquaility(actual: mat.Mat, expected: mat.Mat) {
+    for (const [k,vActual] of Object.entries(actual)) {
+        const vExpected = expected[k as keyof mat.Mat]
+        expect(vActual).approximately(vExpected, epsilon, `Expected element ${k} to be ${vExpected}, but it was ${vActual}`)
+    }
+}
+
+test("Matrix multiplcation", () => {
+    // combine a scaling matrix with a translation matrix
+    const mat1 = mat.make(2, 0, 0, 2, 0, 0)
+    const mat2 = mat.make(1, 0, 0, 1, 10, 20)
+    const result1 = mat.multiply(mat1, mat2)
+    expectMatEquaility(result1, { a: 2, b: 0, c: 0, d: 2, tx: 20, ty: 40 })
+
+    // combine a rotation matrix with a translation matrix
+    const mat3 = mat.make(Math.cos(Math.PI / 2), Math.sin(Math.PI / 2), -Math.sin(Math.PI / 2), Math.cos(Math.PI / 2), 0, 0 )
+    const mat4 = mat.make(1, 0, 0, 1, 10, 20)
+    const result2 = mat.multiply(mat3, mat4)
+    expectMatEquaility(result2, { a: 0, b: 1, c: -1, d: 0, tx: -20, ty: 10 })
+
+})
+
 test("matrix builder", () => {
     const builder = mat.builder()
         .translate(1, 2)
