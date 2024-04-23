e52d52eda746bf645e4f247c83ecf0e50bd656da
Cole Schultz <cole@terrier.tech>
2024-04-23 15:00:55 -0500

    Objects: add dig, bury, and deepMerge and tests
    dig and bury allow for getting and mutating nested objects using index paths
    
    deepMerge deeply merges two objects. Unlike some other deep merge implementations, it merges arrays structurally, rather than using concatenation or replacement

--- src/objects.ts
+++ src/objects.ts
@@ -1,4 +1,5 @@
-import { RequireProps } from "./types"
+import Sets from "./sets"
+import { DeepPartial, ExpandPath, IndexPath, RequireProps, TypeAtPath } from "./types"
 
 /**
  * Creates a shallow copy of `obj` containing only the properties specified in `props`
@@ -18,7 +19,108 @@ function slice<T, K extends keyof T>(obj: T, ...props: K[]): Pick<T, K> {
         return {...obj}
     }
 }
- 
+
+/**
+ * Gets the value of the property at the given nested path
+ * @param obj the object to dig into
+ * @param path the path at which to dig
+ */
+function dig<T extends object, TPath extends IndexPath<T>>(obj: T, path: TPath): TypeAtPath<T, TPath> {
+    let val: any = obj
+    for (const key of path.split('.')) {
+        val = val[key]
+    }
+    return val
+}
+
+/**
+ * Puts the given value at the given index path in the given object
+ */
+function bury<T extends object, Path extends string, ValueType>(
+    obj: T,
+    path: Path,
+    value: ValueType
+): T & ExpandPath<Path, ValueType>  {
+    let val: any = obj
+    const keys = path.split('.')
+    for (let i = 0; i < keys.length - 1; i++) {
+        const key = keys[i]
+        if (!val.hasOwnProperty(key)) {
+            const nextKey = keys[i + 1]
+            if (isNaN(Number(nextKey))) {
+                val[key] = {}
+            } else {
+                val[key] = []
+            }
+        }
+        val = val[key]
+        if (typeof val !== "object") throw new Error(`can't bury in ${typeof val}!`)
+
+    }
+
+    val[keys[keys.length - 1]] = value
+
+    return obj as T & ExpandPath<Path, ValueType>
+}
+
+export type MergeFunc<T> = (a: T | DeepPartial<T>, b: DeepPartial<T>) => T | DeepPartial<T> | undefined | void
+
+/**
+ * Deeply merges two values of the same type. Merges arrays structurally, rather than by concatenation or replacement
+ * Designed for use with non-cyclic, plain old javascript objects.
+ * @param a
+ * @param b
+ * @param customMerge a function to provide custom merge behavior, for instance to handle object types that should be
+ *                    merged as a whole rather than piecemeal.
+ *                    The value returned from this function will be used unless it is undefined or void.
+ */
+function deepMerge<T>(a: T, b: T, customMerge?: MergeFunc<T>): T
+function deepMerge<T>(a: T, b: DeepPartial<T>, customMerge?: MergeFunc<T>): T
+function deepMerge<T>(a: DeepPartial<T>, b: DeepPartial<T>, customMerge?: MergeFunc<T>): DeepPartial<T>
+function deepMerge<T>(a: T | DeepPartial<T>, b: DeepPartial<T>, customMerge?: MergeFunc<T>): T | DeepPartial<T> {
+    if (customMerge) {
+        const customMerged = customMerge(a, b)
+        if (customMerged || customMerged === null) return customMerged
+    }
+
+    if (b === null) {
+        return b
+    } else if (b === undefined) {
+        return deepCopy(a)
+    } else if (a === null || a === undefined) {
+        return deepCopy(b)
+    } else if (Array.isArray(a) && Array.isArray(b)) {
+        const length = a.length > b.length ? a.length : b.length
+        const newArr = [] as unknown[]
+        for (let i = 0; i < length; i++) {
+            newArr[i] = deepMerge(a[i], b[i])
+        }
+        return newArr as DeepPartial<T>
+    } else if (Array.isArray(a) || Array.isArray(b)) {
+        return deepCopy(b)
+    } else if (a instanceof Set && b instanceof Set) {
+        return Sets.union(a, b) as T
+    } else if (a instanceof Map && b instanceof Map) {
+        const newObj = new Map()
+        const keys = Array.from(a.keys()).concat(b.keys())
+        for (const key of keys) {
+            newObj.set(key, deepMerge(a.get(key), b.get(key)))
+        }
+        return newObj as DeepPartial<T>
+    } else if (isPojo(a) && isPojo(b)) {
+        const newObj = {} as Record<string, unknown>
+        const keys = Object.keys(a).concat(Object.keys(b))
+        for (const key of keys) {
+            const aVal = a[key as keyof typeof a] as DeepPartial<T[keyof T]>
+            const bVal = b[key as keyof typeof b] as DeepPartial<T[keyof T]>
+            newObj[key] = deepMerge<T[keyof T]>(aVal, bVal)
+        }
+        return newObj as DeepPartial<T>
+    } else {
+        return deepCopy(b)
+    }
+}
+
  /**
   * Creates a shallow copy of `obj` containing all properties not specified in `props`
   * @param obj the object ot copy
@@ -123,6 +225,9 @@ function safeToString(obj: unknown): string {
 
 const Objects = {
     slice,
+    dig,
+    bury,
+    deepMerge,
     omit,
     omitEmpty,
     shallowCopy,
--- src/test/objects.test.ts
+++ src/test/objects.test.ts
@@ -1,4 +1,4 @@
-import {describe, expect, test} from 'vitest'
+import { describe, expect, test, } from 'vitest'
 import Objects from "../objects"
 
 
@@ -6,6 +6,162 @@ test("slice", () => {
     const obj = {foo: 'one', bar: 'two'}
     const sliced = Objects.slice(obj, 'foo')
     expect(sliced.foo).toBe('one')
+})
+
+describe("dig", () => {
+    type Foo = {
+        bar: number,
+        baz: { qux: string },
+        quz: { garply: boolean }[],
+    }
+
+    const obj: Foo = {
+        bar: 4,
+        baz: { qux: "xyzzy" },
+        quz: [{ garply: true }, { garply: false }]
+    }
+
+    const testCases = [
+        ['bar', 4],
+        ['baz', { qux: "xyzzy" }],
+        ['quz', [{ garply: true }, { garply: false }]],
+        ['baz.qux', "xyzzy"],
+        ['quz.0', { garply: true }],
+        ['quz.1', { garply: false }],
+        ['quz.2', undefined],
+        ['quz.0.garply', true],
+        ['quz.1.garply', false],
+    ] as const
+
+    for (const [key, value] of testCases) {
+        test(`case ${key}`, () => {
+            const actual = Objects.dig(obj, key)
+            expect(actual).toStrictEqual(value)
+        })
+    }
+})
+
+describe("bury", () => {
+    [
+        { path: 'alpha', expected: { alpha: 'value' } },
+        { path: 'alpha.bravo', expected: { alpha: { bravo: 'value' } } },
+        { path: 'alpha.0', expected: { alpha: ['value'] } },
+        { path: 'alpha.2', expected: { alpha: [, , 'value'] } },
+    ].forEach(testCase => {
+        test(`bury ${testCase.path} into empty object`, () => {
+            const actual = Objects.bury({}, testCase.path, 'value')
+            expect(actual).toStrictEqual(testCase.expected)
+        })
+    })
+
+    test('overwrites existing value', () => {
+        const actual = Objects.bury({ alpha: { bravo: 'existing' } }, 'alpha.bravo', 'new value')
+        expect(actual).toStrictEqual({ alpha: { bravo: 'new value' } })
+    })
+
+    test("throws when burying value into non-object", () => {
+        expect(() => Objects.bury({ alpha: 'non-object' }, 'alpha.bravo', 'new value'))
+            .toThrow(/can't bury/)
+    })
+})
+
+describe("deepMerge", () => {
+    test('returns b for primitive values', () => expect(Objects.deepMerge(1, 2)).toBe(2))
+    test('returns b for string values', () => expect(Objects.deepMerge('a', 'b')).toBe('b'))
+
+    test('returns object with properties from a and b', () => {
+        type TestType = { foo: string, bar: string }
+        const a = { foo: 'foo' }
+        const b = { bar: 'bar' }
+        const actual = Objects.deepMerge<TestType>(a, b)
+        expect(actual).toStrictEqual({ foo: 'foo', bar: 'bar' })
+    })
+
+    test('returns object with properties from a and b, overwriting properties from a with b', () => {
+        type TestType = { foo: string, bar: string }
+        const a = { foo: 'foo' }
+        const b = { foo: 'different', bar: 'bar' }
+        const actual = Objects.deepMerge<TestType>(a, b)
+        expect(actual).toStrictEqual({ foo: 'different', bar: 'bar' })
+    })
+
+    test('does not modify either a or b', () => {
+        type TestType = { foo: string, bar: string }
+        const obj1 = { foo: 'foo' }
+        const obj2 = { bar: 'bar' }
+        Objects.deepMerge<TestType>(obj1, obj2)
+        expect(obj1).toStrictEqual({ foo: 'foo' })
+        expect(obj2).toStrictEqual({ bar: 'bar' })
+    })
+
+    test('returns object with deeply nested properties from a and b', () => {
+        type NestedTestType = { foo: { bar: string, baz: string }, qux: string }
+        const a = { foo: { bar: 'a' }, qux: 'a' }
+        const b = { foo: { bar: 'b', baz: 'b' } }
+        const actual = Objects.deepMerge<NestedTestType>(a, b)
+        expect(actual).toStrictEqual({ foo: { bar: 'b', baz: 'b' }, qux: 'a' })
+    })
+
+    test('returns object with null property from a overwritten by property from b', () => {
+        type TestType = { foo: string | null }
+        const a = { foo: null }
+        const b = { foo: 'b' }
+        const actual = Objects.deepMerge<TestType>(a, b)
+        expect(actual).toStrictEqual({ foo: 'b' })
+    })
+
+    test('returns object with property from a overwritten by null property from b', () => {
+        type TestType = { foo: string | null }
+        const a = { foo: 'a' }
+        const b = { foo: null }
+        const actual = Objects.deepMerge<TestType>(a, b)
+        expect(actual).toStrictEqual({ foo: null })
+    })
+
+    test('returns object with undefined property from b not overwriting property from a', () => {
+        type TestType = { foo: string | null | undefined }
+        const a = { foo: 'a' }
+        const b = { foo: undefined }
+        const actual = Objects.deepMerge<TestType>(a, b)
+        expect(actual).toStrictEqual({ foo: 'a' })
+    })
+
+    test('returns object with undefined property from b not overwriting null property from a', () => {
+        type TestType = { foo: string | null | undefined }
+        const a = { foo: null }
+        const b = { foo: undefined }
+        const actual = Objects.deepMerge<TestType>(a, b)
+        expect(actual).toStrictEqual({ foo: null })
+    })
+
+    test('returns array with elements overwritten element by element', () => {
+        const a = [10, 11, 12]
+        const b = [20, 21, 22]
+        const actual = Objects.deepMerge(a, b)
+        expect(actual).toStrictEqual([20, 21, 22])
+    })
+
+    test('returns array with length of a when a is longer', () => {
+        const a = [10, 11, 12, 13]
+        const b = [20, 21, 22]
+        const actual = Objects.deepMerge(a, b)
+        expect(actual).toStrictEqual([20, 21, 22, 13])
+    })
+
+    test('returns array with length of b when b is longer', () => {
+        const a = [10, 11, 12]
+        const b = [20, 21, 22, 23]
+        const actual = Objects.deepMerge(a, b)
+        expect(actual).toStrictEqual([20, 21, 22, 23])
+    })
+
+    test('returns array with merged object elements', () => {
+        type TestType = { foo: string, bar: string }
+        const a = [{ foo: 'a', bar: 'a' }, { foo: 'a' }]
+        const b = [{ bar: 'b' }, { bar: 'b' }, { foo: 'b' }]
+        const actual = Objects.deepMerge<TestType[]>(a, b)
+        expect(actual).toStrictEqual([{ foo: 'a', bar: 'b' }, { foo: 'a', bar: 'b' }, { foo: 'b' }])
+    })
 })
 
 test("omit", () => {
@@ -31,23 +187,32 @@ describe("omitEmpty", () => {
     })
 })
 
-test("Copying", () => {
-    const original = {
-        foo: 'bar',
-        child: {
-            name: 'original'
+describe("Copying", () => {
+    test('modifying nested property of shallow copy affects original', () => {
+        const original = {
+            foo: 'bar',
+            child: {
+                name: 'original'
+            }
         }
-    }
 
-    // make both a deep and shallow copy
-    const shallowCopy = Objects.shallowCopy(original)
-    const deepCopy = Objects.deepCopy(original)
+        const shallowCopy = Objects.shallowCopy(original)
 
-    // modifying the deep copy does not affect the original
-    deepCopy.child.name = 'deep'
-    expect(original.child.name).toBe('original')
+        shallowCopy.child.name = 'shallow'
+        expect(original.child.name).toBe('shallow')
+    })
 
-    // modifying (a nested attributed of) the shallow copy does affect the original
-    shallowCopy.child.name = 'shallow'
-    expect(original.child.name).toBe('shallow')
+    test('modifying nested property of deep copy affects original', () => {
+        const original = {
+            foo: 'bar',
+            child: {
+                name: 'original'
+            }
+        }
+
+        const deepCopy = Objects.deepCopy(original)
+
+        deepCopy.child.name = 'deep'
+        expect(original.child.name).toBe('original')
+    })
 })
